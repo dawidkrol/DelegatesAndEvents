99346761422da45174a3a39f3480e47f37cb846e
Dawid Król <dawidkrol7@gmail.com>
2021-06-06 15:17:37 +0200

    Up#9

--- DelegatesTraining/Counting.cs
+++ DelegatesTraining/Counting.cs
@@ -6,6 +6,13 @@ namespace DelegatesTraining
 {
     public class Counting
     {
+        /// <summary>
+        /// Counting how much monay you need on payday
+        /// </summary>
+        /// <param name="employees">List of employees</param>
+        /// <param name="action">Delegate showing full cost</param>
+        /// <param name="func">Delegate which is counting actual cost</param>
+        /// <returns>Employees cost after special counting</returns>
         public static decimal PayDay(List<Employee> employees, Action<decimal> action, Func<int, decimal, decimal> func)
         {
             decimal fullCost = employees.Sum(x => x.Paymant);
@@ -16,6 +23,12 @@ namespace DelegatesTraining
 
             return output;
         }
+        /// <summary>
+        /// Counting eployees
+        /// </summary>
+        /// <param name="employees">List of employees</param>
+        /// <param name="e">Delegate, which is showing employees</param>
+        /// <returns>Number of employees</returns>
         public static int Employees(List<Employee> employees, Action<Employee> e)
         {
             int output = 0;
--- DelegatesTraining/Employee.cs
+++ DelegatesTraining/Employee.cs
@@ -11,7 +11,7 @@ namespace DelegatesTraining
         public string Name { get; set; }
         public string SurName { get; set; }
         public decimal Paymant { get; set; }
-
+        public static string operator +(Employee a, Employee b) => a.Name + b.Name;
         public override string ToString()
         {
             return $"{Name} {SurName}";
